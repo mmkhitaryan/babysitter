79f9e9c37c163092fad0fd1e5077b9c38701aac0
Marat Mkhitaryan <marat@mkhitaryan.pw>
2023-04-12 15:02:30 +0400

    add vercel to cors

--- babysitter/babysitter/settings.py
+++ babysitter/babysitter/settings.py
@@ -77,6 +77,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://sub.example.com",
     "http://localhost:3000",
     "http://127.0.0.1:9000",
+    "https://babysitter-react.vercel.app",
 ]
 
 ROOT_URLCONF = 'babysitter.urls'
