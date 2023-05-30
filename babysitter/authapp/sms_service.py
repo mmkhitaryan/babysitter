e2a1253fedb3a8c5fb997df6033c8ce11e1bcdb8
Marat Mkhitaryan <marat@mkhitaryan.pw>
2023-05-30 20:59:46 +0400

    append +

--- babysitter/authapp/sms_service.py
+++ babysitter/authapp/sms_service.py
@@ -1,4 +1,4 @@
 import requests
 
 def send_sms(phone, text):
-    requests.get(f"https://smsc.kz/sys/send.php?login=bkzts&psw=9usUWfGv93Hs3Eu&phones={phone}&mes={text}")
+    requests.get(f"https://smsc.kz/sys/send.php?login=bkzts&psw=9usUWfGv93Hs3Eu&phones=+{phone}&mes={text}")
