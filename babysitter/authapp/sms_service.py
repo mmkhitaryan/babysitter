192acdc9ac04aa21fa403993277a583cdc303ce8
Marat Mkhitaryan <marat@mkhitaryan.pw>
2023-05-30 21:41:51 +0400

    sms fix

--- babysitter/authapp/sms_service.py
+++ babysitter/authapp/sms_service.py
@@ -1,5 +1,5 @@
 import requests
 
 def send_sms(phone, text):
-    result = requests.get(f"https://smsc.kz/sys/send.php?login=bkzts&psw=9usUWfGv93Hs3Eu&phones={phone}&mes={text}")
+    result = requests.get(f"https://smsc.kz/sys/send.php?login=bkzts&psw=9usUWfGv93Hs3Eu&phones={phone}&mes={text} BabySitterApp")
     print(result.text, phone, text)
