271260e4733e6dca2628c69a5e91a341047d3b60
Marat Mkhitaryan <marat@mkhitaryan.pw>
2023-06-05 18:56:59 +0400

    add inlines of certificate and reviews on babysitter's admin page

--- babysitter/babysitter/urls.py
+++ babysitter/babysitter/urls.py
@@ -29,22 +29,13 @@ from app.models import Family, Babysitter, BookingTable, Certificate, Review, Ad
 from authapp.models import CustomUser
 from authapp.sms_service import send_sms
 
+class InlineReviewAdmin(admin.TabularInline):
+    model = Review
+    extra = 0
+
 @admin.register(Family)
 class FamilyAdmin(admin.ModelAdmin):
     list_display = ['id','address', 'number_of_children', 'special_needs']
-
-@admin.register(Babysitter)
-class BabysitterAdmin(admin.ModelAdmin):
-    list_display = ['id','hourly_rate', 'years_of_experience', 'bio', 'published', 'full_name', 'detsad','baby','threeToFive', 'birthday']
-    def save_model(self, request, obj, form, change):
-        if change:  # Check if the object is being updated
-            old_obj = self.model.objects.get(pk=obj.pk)
-            if old_obj.published != obj.published:  # Check if the 'approved' field has changed
-                if obj.published:
-                    send_sms(obj.user.phone, f"{obj.full_name} ваш профиль был одобрен")
-                else:
-                    pass
-        super().save_model(request, obj, form, change)
 
 @admin.register(BookingTable)
 class BookingTableAdmin(admin.ModelAdmin):
@@ -54,18 +45,37 @@ class BookingTableAdmin(admin.ModelAdmin):
 class CertificateAdmin(admin.ModelAdmin):
     list_display = ['id','babysitter', 'certificate_file']
 
+class InlineCertificateAdmin(admin.TabularInline):
+    model = Certificate
+    extra = 0
+
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ['id','user_type', 'phone']
 
-
 @admin.register(Review)
-class CertificateAdmin(admin.ModelAdmin):
+class ReviewAdmin(admin.ModelAdmin):
     list_display = ['id','babysitter', 'text', 'rating']
+
 
 @admin.register(Address)
 class AddressAdmin(admin.ModelAdmin):
     list_display = ['id','name']
+
+@admin.register(Babysitter)
+class BabysitterAdmin(admin.ModelAdmin):
+    list_display = ['id','hourly_rate', 'years_of_experience', 'bio', 'published', 'full_name', 'detsad','baby','threeToFive', 'birthday']
+    inlines = [InlineCertificateAdmin,InlineReviewAdmin]
+
+    def save_model(self, request, obj, form, change):
+        if change:  # Check if the object is being updated
+            old_obj = self.model.objects.get(pk=obj.pk)
+            if old_obj.published != obj.published:  # Check if the 'approved' field has changed
+                if obj.published:
+                    send_sms(obj.user.phone, f"{obj.full_name} ваш профиль был одобрен")
+                else:
+                    pass
+        super().save_model(request, obj, form, change)
 
 class AccessUser:
     has_module_perms = has_perm = getattr = lambda s,*a,**kw: True
