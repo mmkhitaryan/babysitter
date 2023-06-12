646b4bbf9eec3b4d4b5b7d275683c3ca5e6d7182
Marat Mkhitaryan <marat@mkhitaryan.pw>
2023-06-12 15:29:39 +0400

    ы

--- backend/babysitter/authapp/sms_service.py
+++ backend/babysitter/authapp/sms_service.py
@@ -4,7 +4,7 @@ SWITCH = True
 
 def send_sms(phone, text):
     if SWITCH:
-        result = requests.get(f"https://smsc.kz/sys/send.php?login=bkzts&psw=9usUWfGv93Hs3Eu&phones={phone}&mes={text} Babysitter App")
+        result = requests.get(f"https://smsc.kz/sys/send.php?login=bkzts&psw=9usUWfGv93Hs3Eu&phones={phone}&mes={text} BabysitterApp")
         print(result.text, phone, text)
     else:
         print(phone, text)
